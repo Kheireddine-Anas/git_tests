4fe9527aefdd620a13cfc00c4d6b4f93acfc727d
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2023-09-17 18:50:39 +0100

    version mahmoud

---
+++ rush/Mahmoud-vr/main.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	rush();
+
+int main()
+{
+	rush(100, 1);
+	return (0);
+}
---
+++ rush/Mahmoud-vr/rush04.c
@@ -0,0 +1,67 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	prtchar(int n, char c)
+{
+	while (n > 0)
+	{
+		ft_putchar(c);
+		n--;
+	}
+}
+
+void	ft_writeligne(int n)
+{
+	if (n > 0)
+		{
+			write(1, "A", 1);
+			prtchar(n - 2,'B');
+		}
+	else
+		{ 
+			write(1, "C", 1);
+			prtchar(-n - 2,'B');
+		}
+	if(n > 0)
+		if(n == 1)//case for colomn
+			ft_putchar('\n');
+		else
+			write(1, "C\n", 2);
+	else
+		if(-n == 1)//case for colomn
+			ft_putchar('\n');
+		else
+			write(1, "A\n", 2);
+}
+
+void	ft_writeemptyligne(int n)
+{
+	write(1, "B", 1);
+	if(n == 1)//case for colomn
+			ft_putchar('\n');
+	else
+		prtchar(n - 2, ' ');
+	if (n >= 2)
+		write(1, "B\n", 2);
+}
+
+int rush(int x, int y)
+{
+	/*if (x > 1000 || y > 1000)
+	{
+		write(1, "Correct The input number\n", 25); //Handling the error message
+		return(0);
+	}*/
+	if(x <= 0 || y <= 0)
+		write(1, "Correct The input number1\n", 25); //return (0);
+	ft_writeligne(x);
+	while (y - 2 > 0)
+	{
+		ft_writeemptyligne(x);
+		y--;
+	}
+	if (y == 2)
+		ft_writeligne(-x);
+	return (0);
+}
