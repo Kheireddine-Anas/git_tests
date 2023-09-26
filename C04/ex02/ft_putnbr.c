eccc161c39983f5c4dcdd204570992c33ceadb60
Anas Kheireddine <akheired@c2r2p2.1337.ma>
2023-09-27 00:58:06 +0100

    The last atoi

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akheired <akheired@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 15:22:01 by akheired          #+#    #+#             */
+/*   Updated: 2023/09/26 15:22:33 by akheired         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_atoi(char *str)
+{
+    int    rzl;
+    int sg;
+
+    rzl = 0;
+    sg = 1;
+    while (*str == ' ' || (*str >= 9 && *str <= 13))
+        str++;
+    while (*str == '-' || *str == '+')
+    {
+        printf("%c1\n", *str);
+        printf("%c1\n", *str);
+        printf("%c1\n", *str-1);
+        if (*str == '-' && *str == '-')
+            sg *= 1;
+        else if (*str == '-' && *str-1 == '+')
+            sg *= -1;
+        str++;
+    }
+    while (*str >= '0' && *str <= '9')
+    {
+        rzl = rzl * 10 + *str - '0';
+        str++;
+    }
+    return (rzl * sg);
+}
+
+int main(int ac, char **av)
+{
+  int ad;
+  int ad1;
+  ad = ft_atoi(av[1]);
+  ad1 = atoi(av[1]);
+  if (ac == 2)
+  {
+    printf("%d\n", ad);
+    printf("%d", ad1);
+  }
+}
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int ft_atoi(char *str)
+// {
+//     int    rzl;
+//     int sg;
+
+//     rzl = 0;
+//     sg = 1;
+//     while (*str == ' ' || (*str >= 9 && *str <= 13))
+//         str++;
+//     if (*str == '-')
+//         sg *= -1;
+//     if (*str == '-' || *str == '+')
+//         str++;
+//     while (*str >= '0' && *str <= '9')
+//     {
+//         rzl = rzl * 10 + *str - '0';
+//         str++;
+//     }
+//     return (rzl * sg);
+// }
+
+// int main(int ac, char **av)
+// {
+//   int ad;
+//   int ad1;
+//   ad = ft_atoi(av[1]);
+//   ad1 = atoi(av[1]);
+//   if (ac == 2)
+//   {
+//     printf("%d\n", ad);
+//     printf("%d", ad1);
+//   }
+// }
